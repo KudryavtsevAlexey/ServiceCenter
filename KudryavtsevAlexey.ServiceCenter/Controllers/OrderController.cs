c0847916c6fc14a48600fe6f2f8cdbcc81b32fab
KudryavtsevAlexey <a.kudryavcev0812@bk.ru>
2021-10-04 20:08:46 +0300

    Added detailed order description

--- KudryavtsevAlexey.ServiceCenter/Controllers/OrderController.cs
+++ KudryavtsevAlexey.ServiceCenter/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using KudryavtsevAlexey.ServiceCenter.Data;
+﻿using AutoMapper;
+using KudryavtsevAlexey.ServiceCenter.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
@@ -8,20 +9,42 @@ namespace KudryavtsevAlexey.ServiceCenter.Controllers
 	public class OrderController : Controller
 	{
 		private readonly ApplicationContext _db;
+		private readonly IMapper _mapper;
 
-		public OrderController(ApplicationContext db)
+		public OrderController(ApplicationContext db, IMapper mapper)
 		{
 			_db = db;
+			_mapper = mapper;
 		}
 
 		public async Task<IActionResult> ManageOrder()
 		{
 			var orders = await _db.Orders
-				.Include(order => order.Client)
-				.Include(order => order.Device)
-				.Include(order => order.Master)
+				.Include(o => o.Client)
+				.Include(o => o.Device)
+				.Include(o => o.Master)
 				.ToListAsync();
 			return View(orders);
+		}
+
+		public async Task<IActionResult> MoreDetailsAsync(int? id)
+		{
+			if (id==null)
+			{
+				return NotFound();
+			}
+
+			var order = await _db.Orders
+				.Include(o => o.Device)
+				.FirstOrDefaultAsync(o => o.OrderId == id);
+				
+
+			if (order!=null)
+			{
+				return View(order);
+			}
+
+			return NotFound();
 		}
 
 		public IActionResult CheckOrderStatus()
